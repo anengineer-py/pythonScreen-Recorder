cd595062761dfbbcdd7d029c2dd0500cee3eacdf
anengineer-py <suleymanbariskoksal@gmail.com>
2021-12-13 22:30:58 +0300

    Create Recording.py

---
+++ Recording.py
@@ -0,0 +1,46 @@
+import cv2
+import pyautogui
+import numpy as np
+
+
+resolution = (1920, 1080)
+
+video = cv2.VideoWriter_fourcc(*"XVID")
+
+filename = "Recording.avi"
+
+fps = 60.0
+
+out = cv2.VideoWriter(filename, video, fps, resolution)
+
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+
+cv2.resizeWindow("Live", 480, 270)
+
+while True:
+
+	# Take screenshot using PyAutoGUI
+	img = pyautogui.screenshot()
+
+	# Convert the screenshot to a numpy array
+	frame = np.array(img)
+
+	# Convert it from BGR(Blue, Green, Red) to
+	# RGB(Red, Green, Blue)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# Write it to the output file
+	out.write(frame)
+	
+	# Optional: Display the recording screen
+	cv2.imshow('Live', frame)
+	
+	# Stop recording when we press 'q'
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+# Release the Video writer
+out.release()
+
+# Destroy all windows
+cv2.destroyAllWindows()
